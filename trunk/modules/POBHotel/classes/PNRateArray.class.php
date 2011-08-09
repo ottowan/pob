41ee6390066b90a8f8ee4ff0cbf377ee6b62c32e
ottowan@gmail.com <ottowan@gmail.com@bbc29f51-810a-595d-9f4c-1017c77577cd>
2011-08-09 01:12:32 +0000

    - Make page for show rate

--- trunk/modules/POBHotel/classes/PNRateArray.class.php
+++ trunk/modules/POBHotel/classes/PNRateArray.class.php
@@ -3,16 +3,26 @@
     function PNRateArray($init=null, $where='') {
       $this->PNObject();
     
-      $this->_objType       = 'pobhotel_rate';
+      $this->_objType       = 'pobhotel_guest_room';
       $this->_objField      = 'id';
       $this->_objPath       = 'form';
+
+      $this->_objJoin[] = array ( 'join_table' => 'pobhotel_guest_room_type',
+                                  'compare_field_table' => 'type_id',
+                                  'compare_field_join' => 'id',
+                                  'join_field' => array('name'),
+                                  'object_field_name' => array('roomtype_name')
+                          );
+
+      $this->_objJoin[] = array ( 'join_table' => 'pobhotel_season',
+                                  'compare_field_table' => 'season_id',
+                                  'compare_field_join' => 'id',
+                                  'join_field' => array('name', 'date_start', 'date_end'),
+                                  'object_field_name' => array('season_name', 'season_date_start', 'season_date_end')
+                          );
       $this->_init($init, $where);
     }
 
-    function genSort(){
-      $order = ' ORDER BY rate_id ASC';
-     return $order;
-    }
   }
 
 
