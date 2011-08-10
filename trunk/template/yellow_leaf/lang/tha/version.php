ede1a286bba85c41fb599df82af4e0a2c5f8a99e
chakanan@gmail.com <chakanan@gmail.com@bbc29f51-810a-595d-9f4c-1017c77577cd>
2011-08-10 03:50:28 +0000

    Add New Template
    name::
    
    blue_hotel
    luxury_hotel
    pink_hotel
    sea_forest
    yellow_leaf

---
+++ trunk/template/yellow_leaf/lang/tha/version.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Zikula Application Framework
+ *
+ * @copyright (c) 2001, Zikula Development Team
+ * @link http://www.zikula.org
+ * @version $Id: version.php,v 1.1 2009/06/26 05:17:33 chongasem Exp $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @author       Mark West
+ * @package      Zikula_Themes
+ * @subpackage   andreas08
+ */
+
+define('_YELLOW_LEAF_THEME_NAME', 'Yellow Leaf');
+define('_YELLOW_LEAF_DISPLAYNAME', 'Yellow Leaf');
+define('_YELLOW_LEAF_DESCRIPTION', 'Yellow Leaf');
