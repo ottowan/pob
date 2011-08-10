d23e0de6281bb427c1460aa45d61e5133982e073
Ottowan <ottowan@gmail.com>
2011-08-10 10:00:50 +0000

    - Add translate

---
+++ modules/POBRoomSearch/pnlang/eng/user.php
@@ -0,0 +1,9 @@
+<?php
+//user_list_topics Definitions
+  define('_POBROOMSEARCH_USER_RESULT','result');
+  define('_POBROOMSEARCH_USER_NO_RESULT','Result not found.');
+
+
+  //block_searchroomleft.htm
+  define('_POBROOMSEARCH_USER_CHECK_IN_DATE','Check-in date');
+?>
---
+++ modules/POBRoomSearch/pnlang/tha/user.php
@@ -0,0 +1,9 @@
+<?php
+//user_list_topics Definitions
+  define('_POBROOMSEARCH_USER_RESULT','ผลการค้นหา');
+  define('_POBROOMSEARCH_USER_NO_RESULT','ไม่มีข้อมูล');
+
+  //block_searchroomleft.htm
+  define('_POBROOMSEARCH_USER_CHECK_IN_DATE','วันที่เช็คอิน');
+
+?>
