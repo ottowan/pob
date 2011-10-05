f689ea12b06744c89b79672cab0c1627477c662c
ottowan@gmail.com <ottowan@gmail.com@bbc29f51-810a-595d-9f4c-1017c77577cd>
2011-10-05 17:38:59 +0000

    Make new module for manage slide images

---
+++ trunk/modules/Gallery/pnversion.php
@@ -0,0 +1,15 @@
+<?php
+$modversion['name'] = 'Gallery';
+$modversion['displayname'] = 'Gallery';
+$modversion['description'] = ' Gallery module';
+$modversion['version'] = '1.0';
+$modversion['credits'] = 'pndocs/credits.txt';
+$modversion['help'] = 'pndocs/install.txt';
+$modversion['changelog'] = 'pndocs/changelog.txt';
+$modversion['license'] = 'pndocs/license.txt';
+$modversion['official'] = 1;
+$modversion['author'] = 'Parinya Bumrungchoo';
+$modversion['contact'] = 'http://www.phuketinnova.com';
+$modversion['admin'] = 0;
+$modversion['securityschema'] = array();
+?>
